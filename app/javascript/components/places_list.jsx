6fdf870745b1eced25313829b0d4cfc25d1635c7
luanmachad0 <luansilvamachado@gmail.com>
2022-08-29 16:29:30 -0300

    Add basic places list working using reactjs
    This hits a /api/places endpoint and gets the data we need to show
    on the screen!

--- app/javascript/components/places_list.jsx
+++ app/javascript/components/places_list.jsx
@@ -1,13 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
 
-class PlacesList extends React.Component {
-    render() {
-        return (
-            <div>Places list rendered in react!</div>
-        )
+function PlacesList() {
+    const [loading, setLoading] = useState(true);
+    const [loadedPlaces, setLoadedPlaces] = useState([]);
+
+    useEffect(() => {
+        const apiEndpoint = "/api/places"
+
+        fetch(apiEndpoint)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                setLoadedPlaces(data["places"])
+                setLoading(false)
+            })
+    }, [])
+
+    const loadingSection = (<div>Loading...</div>);
+    const dataSection = loadedPlaces.map((place, index) =>
+        <div key={index}>
+            <table>
+                <tr>
+                    <th>Name</th>
+                    <th>City</th>
+                    <th>Recent Upload Speed</th>
+                    <th>Recent Upload Speed Units</th>
+                    <th>Number of measurements</th>
+                </tr>
+                <tr>
+                    <td>{place.name}</td>
+                    <td>{place.city}</td>
+                    <td>{place.most_recent_download_speed}</td>
+                    <td>{place.most_recent_download_units}</td>
+                    <td>{place.number_of_measurements}</td>
+                </tr>
+            </table>
+        </div>
+    )
+
+    if (loading) {
+        return loadingSection
+    } else {
+        return dataSection
     }
 }
+
 
 const placesList = ReactDOM.createRoot(document.getElementById("places-list-container"));
 placesList.render(<PlacesList />);
