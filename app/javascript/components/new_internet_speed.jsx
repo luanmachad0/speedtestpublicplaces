9f019ed84ebf5b9897c81b1c7ee0007f0cc4fa03
luanmachad0 <luansilvamachado@gmail.com>
2022-09-25 17:27:40 -0300

    Updated routes to default to react controller
    This will allow us to go to a page like /new-internet-speed directly
    without rails complaining that the route does not exist.
    Essentially, all routing delegated to ReactRouter.

--- app/javascript/components/new_internet_speed.jsx
+++ app/javascript/components/new_internet_speed.jsx
@@ -1,7 +1,23 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 export default function NewInternetSpeed() {
   return (
-    <div> Placeholder page to enter new internet speed</div>
+    <div className="bg-white p-8 rounded-md w-full">
+      <div className=" flex items-center justify-between pb-6">
+          <div>
+              <h2 className="text-4xl text-gray-600 font-semibold">Places</h2>
+          </div>
+          <div className="flex items-center justify-between">
+              <div className="lg:ml-40 ml-10 space-x-8">
+                  <Link to="/new-internet-speed">
+                      <button className="bg-indigo-600 px-4 py-2 rounded-md text-white font-semibold tracking-wide cursor-pointer">
+                          New Log
+                      </button>
+                  </Link>
+              </div>
+          </div>
+      </div>
+    </div>
   )
 }
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "places#index"
-
   get "/api/places", to: "api/places#index"
+
+  get "*path", to: "react#home"
+  root "react#home"
 end
