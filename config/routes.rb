baa3d77d278185c3002e3dde563c23bfd901387f
luanmachad0 <luansilvamachado@gmail.com>
2022-09-26 10:45:45 -0300

    Implements functionality to capture new internet speeds
    User enters place name, address and city, then clicks the "start speed
    test" button. This will trigger a speed test that downloads an image 5
    times and logs the speed.
    
    Place info + avarage speed is then uploaded to the server via a POST
    /api/internet-speed request which creates both the `place` and the avg
    speed and units.
    
    Next up, we'll on some refining these features and making more
    relisient.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   get "/api/places", to: "api/places#index"
+  post "/api/internet_speed", to: "api/internet_speed#create"
 
   get "*path", to: "react#home"
   root "react#home"
